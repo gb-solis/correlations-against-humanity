67dfaa6452975491abc921c161358624e9788004
gb-solis <gabriel.solis@usp.br>
2021-08-05 23:42:56 -0300

    rascunho de um menu

--- humanity-against-bot.py
+++ humanity-against-bot.py
@@ -1,6 +1,7 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from random import randint
 import logging
+from random import randint
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from cards_against_humanity import parser
 
 # lidar com erros
@@ -33,7 +34,7 @@ def patada(update, context):
                f'Fica de boa ai {nome}',
                f'cansou de desapontar a família {sobrenome} e veio encher o saco',
                f'{nome} {sobrenome}: um contrargumento para a liberdade de expressão',
-               3*'AAAAAAAAAAAAAAAAAAAAAAAAAA',
+               2*'AAAAAAAAAAAAAAAAAAAAAAAAAA',
                'cringe',
                'cringe')
     índice = randint(0, len(frases)-1)
@@ -46,12 +47,10 @@ def cards_against_humanity_bot(update, context):
     dados conformemente'''
     path_mensagens = 'mensagens.txt'
     path_histórico = 'histórico.txt'
-    # path_chutes = 'chutes.txt'
-    # path_pontos_chutes = 'chutes_pontos.txt'
     
     mensagem = update.message.text
     rodada = parser(mensagem)
-    # anota o vencedor (deveríamos anotar o czar também, mas como?)
+    # anota o vencedor (deveríamos anotar o czar também...)
     if rodada.finalizada:
         vencedor = rodada.vencedor
         with open(path_histórico, 'a') as file:
@@ -59,7 +58,7 @@ def cards_against_humanity_bot(update, context):
     # salva as mensagens do bot para análise posterior
     if rodada.finalizada or rodada.recebida:
         with open(path_mensagens, 'a') as file:
-            file.write(mensagem + '\n')
+            file.write(mensagem + '\n\n')
 
 
 def conversa(update, context):
@@ -71,25 +70,94 @@ def conversa(update, context):
     elif update.message.text[-1] == '?':
         update.message.reply_text('sua mãe')
 
-
 def erro(update, context):
     update.message.reply_text('Ocorreu um erro!')
 
 
-############### corpo do programa ##################################
+
+################################# menu ########################################
+
+# uma finite-state machine
+MENU, ESCOLHA, FIM = range(3)
+
+def início(update, context):
+    '''mensagem de boas-vindas ao usuário. Resposta ao comando /start'''
+    nome = update.message.from_user.first_name
+    update.message.reply_text(f"Yoo {nome}, fala ae")
+    print('mandei pro menu')
+    return MENU
+
+
+def menu(update, context):
+    print('menu')
+    entradas = (('chutar vencedor',), ('reclamar do último czar',),)
+    update.message.reply_text('O que quer de mim?',
+        reply_markup=ReplyKeyboardMarkup(entradas, one_time_keyboard=True,))
+    return ESCOLHA
+    
+
+def chutar(update, context):
+    opções = (('1. sua mãe',), ('2. minha carta',), ('3. nenhuma',),)
+    update.message.reply_text('Que carta você acha que vai ganhar?',
+        reply_markup=ReplyKeyboardMarkup(opções, one_time_keyboard=True,))
+    return FIM
+
+
+def reclamar(update, context):
+    update.message.reply_text('Pode desabafar')
+    return MENU
+    # return ConversationHandler.END
+    
+
+def fim(update, context):
+    path_chutes = 'chutes.txt'
+    chute = update.message.text
+    print(chute)
+    nome = update.message.from_user.first_name
+    print(nome)
+    with open(path_chutes, 'a') as file:
+        file.write(f'{nome}: {chute}\n')
+    return MENU
+    # return ConversationHandler.END
+
+
+def cancelar(update, context):
+    update.message.reply_text('#cancelado')
+    return ConversationHandler.END
+
+
+menu_handler = ConversationHandler(
+    entry_points=[CommandHandler('menu', menu)],
+    # entry_points=[CommandHandler('start', início)],
+    states={#MENU: MessageHandler(Filters.all, menu)],
+            ESCOLHA: [MessageHandler(Filters.regex(r'chutar'), chutar),
+                      MessageHandler(Filters.regex(r'reclamar'), reclamar)],
+            FIM: [MessageHandler(Filters.all, fim)]},
+    fallbacks=[CommandHandler('cancelar', cancelar)]
+    )
+
+
+
+############### corpo do programa #############################################
 def main():
     # setup inicial, só aceita
     updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
 
-    # add handlers for start and help commands
-    dispatcher.add_handler(CommandHandler("start", start))
+    # comandos "start" e "help"
+    # dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", ajuda))
 
-    # add a handler for normal text (not commands)
+    # comando "chute"
+    dispatcher.add_handler(CommandHandler("chutar", chutar))
+    
+    # menu
+    dispatcher.add_handler(menu_handler)
+    
+    # handler pra texto normal, sem comandos
     dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command),
                                           conversa))
-
+    
     # add a handler for errors
     dispatcher.add_error_handler(erro)
 
